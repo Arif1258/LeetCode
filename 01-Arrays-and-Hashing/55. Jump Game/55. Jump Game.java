c22e0aac0e3915e00d1523ae6afe3c3fdea7f7d1
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-07-28 15:45:38 +0530

    [55] [Time Beats: 88.86%] [Memory Beats: 92.16%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/55. Jump Game/55. Jump Game.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int maxReach = 0;
+4        for(int i=0; i<nums.length;i++){
+5            if(i > maxReach) return false;
+6            maxReach = Math.max(maxReach, i+nums[i]);
+7
+8            if(maxReach >= nums.length-1) return true;
+9        }
+10        return true;
+11    }
+12}
