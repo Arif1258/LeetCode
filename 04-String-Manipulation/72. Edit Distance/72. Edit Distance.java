a19ddf9aa1f117d133f34fcec86f13ac685de284
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-08-10 20:58:29 +0530

    [72] [Time Beats: 8.69%] [Memory Beats: 35.55%] - Leet2Hub

---
+++ 04-String-Manipulation/72. Edit Distance/72. Edit Distance.java
@@ -0,0 +1,29 @@
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int m = word1.length();
+4        int n = word2.length();
+5        int[][] dp = new int[m + 1][n + 1];
+6        for (int i = 0; i <= m; i++) {
+7            dp[i][0] = i;
+8        }
+9        for (int j = 0; j <= n; j++) {
+10            dp[0][j] = j;
+11        }
+12        for (int i = 1; i <= m; i++) {
+13            for (int j = 1; j <= n; j++) {
+14                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+15                    dp[i][j] = dp[i - 1][j - 1];
+16                } else {
+17                    dp[i][j] = 1 + Math.min(dp[i - 1][j],  
+18                        Math.min(
+19                            dp[i][j - 1],  
+20                            dp[i - 1][j - 1] 
+21                        )
+22                    );
+23                }
+24            }
+25        }
+26
+27        return dp[m][n];
+28    }
+29}
