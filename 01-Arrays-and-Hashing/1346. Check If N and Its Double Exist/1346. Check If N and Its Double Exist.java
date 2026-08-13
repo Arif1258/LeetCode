1df5bb35621690dea837453d5689d9432705a591
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-08-13 21:20:42 +0530

    [1346] [Time Beats: 97.51%] [Memory Beats: 57.80%] - Leet2Hub

--- 01-Arrays-and-Hashing/1346. Check If N and Its Double Exist/1346. Check If N and Its Double Exist.java
+++ 01-Arrays-and-Hashing/1346. Check If N and Its Double Exist/1346. Check If N and Its Double Exist.java
@@ -1,1 +1,13 @@
-class Solution {    public boolean checkIfExist(int[] arr) {        for(int i=0;i<arr.length;i++){            for(int j=arr.length-1;j>=0;j--){                if(i != j && arr[i] == 2*arr[j]) return true;            }        }        return false;    }}
+1class Solution {
+2    public boolean checkIfExist(int[] arr) {
+3        HashSet<Integer> set = new HashSet<>();
+4        for (int num : arr) {
+5            if (set.contains(2 * num) || (num % 2 == 0 && set.contains(num / 2))){
+6                return true;
+7            }
+8            set.add(num);
+9        }
+10
+11        return false;
+12    }
+13}
