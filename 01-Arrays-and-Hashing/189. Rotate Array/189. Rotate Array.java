6eb46812cf70e16add24bec7ec59b5436cc2e19c
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-08-19 11:02:41 +0530

    [189] [Time Beats: 4.17%] [Memory Beats: 8.00%] - Leet2Hub

---
+++ 01-Arrays-and-Hashing/189. Rotate Array/189. Rotate Array.java
@@ -0,0 +1,21 @@
+1class Solution {
+2    public void rotate(int[] nums, int k) {
+3        int n = nums.length;
+4        k = k % n;
+5
+6        reverse(nums, 0, n - 1);
+7        reverse(nums, 0, k - 1);
+8        reverse(nums, k, n - 1);
+9    }
+10
+11    private void reverse(int[] nums, int left, int right) {
+12        while (left < right) {
+13            int temp = nums[left];
+14            nums[left] = nums[right];
+15            nums[right] = temp;
+16
+17            left++;
+18            right--;
+19        }
+20    }
+21}
