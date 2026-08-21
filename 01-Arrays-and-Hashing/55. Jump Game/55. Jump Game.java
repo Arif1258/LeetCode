fe3bf52c793013bd1971572994dc909ec47ee734
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-08-21 10:56:18 +0530

    [55] [Time Beats: 88.90%] [Memory Beats: 43.27%] - Leet2Hub

--- 01-Arrays-and-Hashing/55. Jump Game/55. Jump Game.java
+++ 01-Arrays-and-Hashing/55. Jump Game/55. Jump Game.java
@@ -4,9 +4,7 @@
 4        for(int i=0; i<nums.length;i++){
 5            if(i > maxReach) return false;
 6            maxReach = Math.max(maxReach, i+nums[i]);
-7
-8            if(maxReach >= nums.length-1) return true;
-9        }
-10        return true;
-11    }
-12}
+7        }
+8        return true;
+9    }
+10}
