6fdb04371c31e6051d0771ccb79e38ce5e10e5ee
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-09-01 20:24:23 +0530

    [518] [Time Beats: N/A] [Memory Beats: N/A] - Leet2Hub

--- 01-Arrays-and-Hashing/518. Coin Change II/518. Coin Change II.java
+++ 01-Arrays-and-Hashing/518. Coin Change II/518. Coin Change II.java
@@ -1,14 +1,1 @@
-1class Solution {
-2    public int change(int amount, int[] coins) {
-3        int dp[] = new int [amount+1];
-4
-5        dp[0] = 1;
-6
-7        for(int coin : coins){
-8            for(int i=coin; i<=amount; i++){
-9                dp[i] += dp[i-coin];
-10            }
-11        }
-12        return dp[amount];
-13    }
-14}
+class Solution {    public int change(int amount, int[] coins) {        int dp[] = new int [amount+1];        dp[0] = 1;        for(int coin : coins){            for(int i=coin; i<=amount; i++){        }                dp[i] += dp[i-coin];            }    }        return dp[amount];}
