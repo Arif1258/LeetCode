4eb76c7d61a8ed2d22a86412d42de6ea675f1f80
Sk Arif Ahmed <arifahmedma490@gmail.com>
2026-09-12 11:26:59 +0530

    [136] [Time Beats: 99.94%] [Memory Beats: 91.91%] - Leet2Hub

--- 01-Arrays-and-Hashing/136. Single Number/136. Single Number.java
+++ 01-Arrays-and-Hashing/136. Single Number/136. Single Number.java
@@ -1,9 +1,11 @@
 1class Solution {
 2    public int singleNumber(int[] nums) {
-3        int ans = 0;
-4        for(int i = 0;i<nums.length;i++){
-5            ans = ans^nums[i];
-6        }
-7        return ans;
-8    }
-9}
+3        int n = 0;
+4
+5        for(int i=0; i<nums.length;i++){
+6            n = n ^ nums[i];
+7
+8        }
+9        return n;
+10    }
+11}
